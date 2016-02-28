b18d7a66156549d0751de5eb5c917e81dc6cde0a
Sjúrður í Sandagerði <sjurdur@Sjurdurs-MacBook-Pro.local>
2016-02-28 23:12:06 +0100

    Bit of code duplication, but now there is only one landscape mode

--- TingleV4/app/src/main/java/mmad/sjurdur/tingle/ListActivity.java
+++ TingleV4/app/src/main/java/mmad/sjurdur/tingle/ListActivity.java
@@ -1,5 +1,6 @@
 package mmad.sjurdur.tingle;
 
+import android.content.res.Configuration;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -10,18 +11,33 @@ import android.widget.ListView;
 
 public class ListActivity extends FragmentActivity {
 
+    private Fragment activity_fragment;
+    private Fragment list_fragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
         FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.activity_list_fragment_container);
+        list_fragment = fm.findFragmentById(R.id.activity_list_fragment_container);
 
-        if (fragment == null) {
-            fragment = new ListFragment();
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+
+            activity_fragment = fm.findFragmentById(R.id.activity_tingle_fragment_container);
+            if (activity_fragment == null) {
+                activity_fragment = new TingleFragment();
+                fm.beginTransaction()
+                        .add(R.id.activity_tingle_fragment_container, activity_fragment)
+                        .commit();
+            }
+        }
+
+        list_fragment = fm.findFragmentById(R.id.activity_list_fragment_container);
+        if (list_fragment == null) {
+            list_fragment = new ListFragment();
             fm.beginTransaction()
-                    .add(R.id.activity_list_fragment_container, fragment)
+                    .add(R.id.activity_list_fragment_container, list_fragment)
                     .commit();
         }
 
